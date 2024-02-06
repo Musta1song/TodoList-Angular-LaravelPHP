6a5f948d3957abae12b117f428296724ac4a471e
Musta1song <147011583+Musta1song@users.noreply.github.com>
2024-02-06 16:59:59 -0500

    update

--- src/app/components/todolist/todolist.component.ts
+++ src/app/components/todolist/todolist.component.ts
@@ -19,116 +19,122 @@ import {
   transferArrayItem,
   DragDropModule
 } from '@angular/cdk/drag-drop';
+
 import { TodoListPage2 } from '../../classes/todo-list-page2';
 
 
 @Component({
   selector: 'app-todolist',
   standalone: true,
-  imports: [CdkDropListGroup, CdkDropList, CdkDrag,CommonModule, RouterOutlet, MatCardModule, FormsModule, MatButtonModule, MatIconModule,DragDropModule],
+  imports: [CdkDropListGroup, CdkDropList, CdkDrag, CommonModule, RouterOutlet, MatCardModule, FormsModule, MatButtonModule, MatIconModule, DragDropModule],
 
   templateUrl: './todolist.component.html',
   styleUrl: './todolist.component.scss'
 })
-export class TodolistComponent implements OnInit{
+export class TodolistComponent implements OnInit {
 
-title = 'AtodoNew';
-done!: TodoListPage2[];
-todo!: TodoListPage2[];
+  title = 'AtodoNew';
+  done!: TodoListPage2[];
+  todo!: TodoListPage2[];
 
-constructor(
-  public deleteService: DeleteService,
-  public getTodoListService: GetTodoListService,
-  public isDoneService: MarkTodoAsDoneService) { }
-todolist!: TodoList[]
-todos: TodoList = new TodoList()
-id!: number
+  constructor(
+    public deleteService: DeleteService,
+    public getTodoListService: GetTodoListService,
+    public isDoneService: MarkTodoAsDoneService) { }
+  todolist!: TodoList[]
+  todos: TodoList = new TodoList()
+  id!: number
 
 
 
-drop(event: CdkDragDrop<any[]>) {
-  if (event.previousContainer === event.container) {
-    moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-  } else {
-    transferArrayItem(
-      event.previousContainer.data,
-      event.container.data,
-      event.previousIndex,
-      event.currentIndex,
-      
-    );
-    document.getElementById("matbt")!.style.visibility = "visible"
+  drop(event: CdkDragDrop<any[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
 
+      );
+      document.getElementById("matbt")!.style.visibility = "visible"
+
+    }
   }
-}
-deleteTodo(ID:any){
-  this.deleteService.deleteTodo(ID).subscribe();
-      window.location.reload()
-}
+  deleteTodo(ID: any) {
+    this.deleteService.deleteTodo(ID).subscribe();
+    setTimeout(this.reload, 500
 
-markTodoAsDone() {
-  for (let i of this.done) {
-    if (i.isDone != true)
-      this.isDoneService.updateTodo(i.id).subscribe();
+      ) 
   }
-      window.location.reload()
-  
 
-}
-ngOnInit(): void {
-  this.getTodoListService.GetTodoList().subscribe((data: TodoList[]) => {
-    this.todolist = data;
-    this.sort(this.todolist)
-    this.sortByIsDone(this.todolist)
-    console.log(this.todo)
-  });
-}
+  markTodoAsDone() {
+    for (let i of this.done) {
+      if (i.isDone != true)
+        this.isDoneService.updateTodo(i.id).subscribe();
+    }
+    setTimeout(this.reload, 500
 
-sort(arr: any[]) {
-  let timeArr: any[] = this.getTimeArr(arr);
-  let size = arr.length;
-  let minIndex;
-  for (let i = 0; i < size - 1; i++) {
-    minIndex = i;
-    for (let j = i; j < size; j++) {
-      if (timeArr[minIndex] > timeArr[j]) {
-        minIndex = j;
+    )
+  }
+  reload(){
+    window.location.reload()
+  }
+  ngOnInit(): void {
+    this.getTodoListService.GetTodoList().subscribe((data: TodoList[]) => {
+      this.todolist = data;
+      this.sort(this.todolist)
+      this.sortByIsDone(this.todolist)
+      console.log(this.todo)
+    });
+  }
+
+  sort(arr: any[]) {
+    let timeArr: any[] = this.getTimeArr(arr);
+    let size = arr.length;
+    let minIndex;
+    for (let i = 0; i < size - 1; i++) {
+      minIndex = i;
+      for (let j = i; j < size; j++) {
+        if (timeArr[minIndex] > timeArr[j]) {
+          minIndex = j;
+        }
+      }
+      if (i !== minIndex) {
+        let temp = timeArr[i];
+        timeArr[i] = timeArr[minIndex];
+        timeArr[minIndex] = temp;
+        let temp2 = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp2
       }
     }
-    if (i !== minIndex) {
-      let temp = timeArr[i];
-      timeArr[i] = timeArr[minIndex];
-      timeArr[minIndex] = temp;
-      let temp2 = arr[i];
-      arr[i] = arr[minIndex];
-      arr[minIndex] = temp2
+    return arr;
+  }
+  getTimeArr(arr: any[]) {
+    let timeArr: any = [
+    ]
+    for (let i of arr) {
+      timeArr.push(i.time)
     }
+    return timeArr
   }
-  return arr;
-}
-getTimeArr(arr: any[]) {
-  let timeArr: any = [
-  ]
-  for (let i of arr) {
-    timeArr.push(i.time)
-  }
-  return timeArr
-}
 
-sortByIsDone(arr: any[]) {
-  let done: any = []
-  let todo: any = []
+  sortByIsDone(arr: any[]) {
+    let done: any = []
+    let todo: any = []
 
-  for (let i of arr) {
-    if (i.isDone == true) {
-      done.push(i)
+    for (let i of arr) {
+      if (i.isDone == true) {
+        done.push(i)
+      }
+      else {
+        todo.push(i)
+      }
     }
-    else {
-      todo.push(i)
-    }
+    this.done = done
+    this.todo = todo
   }
-  this.done = done
-  this.todo = todo
-}
 
 }
