e24324944ea016d62643797447bb675cc150107f
Mustafa <mustafa@pop-os.localdomain>
2024-04-21 19:47:17 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/app/components/{todolist,addtodo}.{html,ts}",
-"./src/**/*.{html,ts}"],
+  content: ["./src/**/**/*.{html,ts}"],
   theme: {
     extend: {},
   },
